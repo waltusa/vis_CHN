a88f9a267578b895b587ce6d5fd15f1ec7d977a7
zhenyi-wang <zhenyi.wang@live.com>
2020-09-08 14:24:27 +0800

    minor modification

--- walt_chn/js/real_stop_table.js
+++ walt_chn/js/real_stop_table.js
@@ -1,74 +1,73 @@
-
-
-function realTime_error_table(error_data){
+function realTime_error_table(error_data) {
     $("#realTime_error_body").empty();
     let content_html = '';
-    let new_error_data = error_data.filter(item=>item['StopCode']!=99992)
-    if(new_error_data.length > 8){
+    let new_error_data = error_data.filter(item => item['StopCode'] != 99992)
+        // if(new_error_data.length > 8){
+    if (false) { //暂时关闭报警
         let playPromise = new Audio('alarm.mp3');
         playPromise.play().then(() => {
-            console.log('Playing')
-        })
+                console.log('Playing')
+            })
             .catch(e => {
                 console.log('Error Occured', e.message);
             })
     }
-    let filter_error_data =[]
-    for (let i in error_data){
-        if(error_data[i]['StopCode']==99992){
+    let filter_error_data = []
+    for (let i in error_data) {
+        if (error_data[i]['StopCode'] == 99992) {
             continue;
-        }else{
+        } else {
             filter_error_data.push(error_data[i])
         }
     }
 
-    for(let i = 0;i<filter_error_data.length;i++){
+    for (let i = 0; i < filter_error_data.length; i++) {
         let date = filter_error_data[i]['DateRec'].slice(10, filter_error_data[i]['DateRec'].length)
-        let content ='<tr><td>'+'#'+filter_error_data[i]['MachCode']+'</td><td>' + filter_error_data[i]['StopCode'] + '</td><td>' + date+'</td><td>' + filter_error_data[i]['description']+'</td></tr>';
+        let content = '<tr><td>' + '#' + filter_error_data[i]['MachCode'] + '</td><td>' + filter_error_data[i]['StopCode'] + '</td><td>' + date + '</td><td>' + filter_error_data[i]['description'] + '</td></tr>';
         content_html += content + '\n';
     }
-    document.getElementById("realTime_error_body").innerHTML =content_html;
+    document.getElementById("realTime_error_body").innerHTML = content_html;
 }
 
 
-function realTime_error_bar(fixedData,errorData){
+function realTime_error_bar(fixedData, errorData) {
 
     let error_data_dict = {}
-    for(let i in fixedData){
+    for (let i in fixedData) {
         let obj = fixedData[i];
-        if(obj['StopCode'] in error_data_dict){
-            error_data_dict[obj['StopCode']]['fixed']+=1;
-        } else{
-            error_data_dict[obj['StopCode']] = {'StopCode':obj['StopCode'],'Description':obj['description'],'fixed':1,'unfixed':0}
+        if (obj['StopCode'] in error_data_dict) {
+            error_data_dict[obj['StopCode']]['fixed'] += 1;
+        } else {
+            error_data_dict[obj['StopCode']] = { 'StopCode': obj['StopCode'], 'Description': obj['description'], 'fixed': 1, 'unfixed': 0 }
         }
     }
-    for(let j in errorData){
+    for (let j in errorData) {
         let e_obj = errorData[j];
-        if(e_obj['StopCode'] in error_data_dict){
-            error_data_dict[e_obj['StopCode']]['unfixed']+=1;
-        } else{
-            error_data_dict[e_obj['StopCode']] = {'StopCode':e_obj['StopCode'],'Description':e_obj['description'],'fixed':0,'unfixed':1}
+        if (e_obj['StopCode'] in error_data_dict) {
+            error_data_dict[e_obj['StopCode']]['unfixed'] += 1;
+        } else {
+            error_data_dict[e_obj['StopCode']] = { 'StopCode': e_obj['StopCode'], 'Description': e_obj['description'], 'fixed': 0, 'unfixed': 1 }
         }
 
     }
-    let new_error_data_dict={}
+    let new_error_data_dict = {}
     let total_count = 0;
-    for(let m in error_data_dict){
-        total_count+=error_data_dict[m]['fixed'];
-        total_count+=error_data_dict[m]['unfixed'];
+    for (let m in error_data_dict) {
+        total_count += error_data_dict[m]['fixed'];
+        total_count += error_data_dict[m]['unfixed'];
 
-        if(error_data_dict[m]['fixed']>5 || error_data_dict[m]['unfixed']>0){
+        if (error_data_dict[m]['fixed'] > 5 || error_data_dict[m]['unfixed'] > 0) {
             new_error_data_dict[m] = error_data_dict[m]
         }
     }
 
-    let text = '实时错误总计发生：'+total_count;
-    let res_counts = Object.keys(new_error_data_dict).sort(function(a,b){
-        return new_error_data_dict[a]['fixed']-new_error_data_dict[b]['fixed'];
+    let text = '实时错误总计发生：' + total_count;
+    let res_counts = Object.keys(new_error_data_dict).sort(function(a, b) {
+        return new_error_data_dict[a]['fixed'] - new_error_data_dict[b]['fixed'];
     });
     let x_fixed_data = [];
     let x_unfixed_data = [];
-    res_counts.forEach(function(d){
+    res_counts.forEach(function(d) {
         let item = new_error_data_dict[d]
         x_fixed_data.push(parseInt(item['fixed']));
         x_unfixed_data.push(parseInt(item['unfixed']))
@@ -83,10 +82,10 @@ function realTime_error_bar(fixedData,errorData){
     let option = {
         title: {
             text: text,
-            subtext:'仅展示已修复故障次数 > 5 或者正在发生故障',
+            subtext: '仅展示已修复故障次数 > 5 或者正在发生故障',
             left: 'center',
-            textStyle:{//标题内容的样式
-                color:'dodgerblue',
+            textStyle: { //标题内容的样式
+                color: 'dodgerblue',
 
             },
         },
@@ -95,40 +94,40 @@ function realTime_error_bar(fixedData,errorData){
             trigger: 'axis',
             showDelay: 20,
             hideDelay: 20,
-            transitionDuration:0,
-            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            transitionDuration: 0,
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
             },
-            formatter: function (params) {
+            formatter: function(params) {
                 return new_error_data_dict[params[0]['name']]['Description']
             }
         },
         legend: {
             data: ['已修复故障', '故障正在发生'],
-             top:"14%",
-            textStyle:{//图例文字的样式
-                color:'dodgerblue',
-                fontSize:12
+            top: "14%",
+            textStyle: { //图例文字的样式
+                color: 'dodgerblue',
+                fontSize: 12
             }
         },
         grid: {
             left: '3%',
             right: '4%',
             bottom: '3%',
-            top:'25%',
+            top: '25%',
             containLabel: true
         },
         xAxis: {
             type: 'value',
             splitLine: {
                 lineStyle: {
-                    color:['#22cab7']
+                    color: ['#22cab7']
                 }
             },
-            axisLine:{
-                lineStyle:{
-                    color:'#22cab7',
-                    width:2,
+            axisLine: {
+                lineStyle: {
+                    color: '#22cab7',
+                    width: 2,
                 }
             }
 
@@ -138,22 +137,21 @@ function realTime_error_bar(fixedData,errorData){
             type: 'category',
             data: res_counts,
             axisLabel: {
-            show: true, //默认为true，设为false后下面都没有意义了
-            interval: 0, //此处关键， 设置文本标签全部显示
+                show: true, //默认为true，设为false后下面都没有意义了
+                interval: 0, //此处关键， 设置文本标签全部显示
             },
-            nameTextStyle:{
-        color:['#be7012']
-    },
+            nameTextStyle: {
+                color: ['#be7012']
+            },
             axisLine: {
-            lineStyle:{
-            color:'#ebf8ac',
-                width:2
-        },
+                lineStyle: {
+                    color: '#ebf8ac',
+                    width: 2
+                },
 
-    }
+            }
         },
-        series: [
-            {
+        series: [{
                 name: '已修复故障',
                 type: 'bar',
                 stack: '总量',
@@ -166,9 +164,9 @@ function realTime_error_bar(fixedData,errorData){
                     normal: {
                         barBorderRadius: 5,
                         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                            offset: 0,
-                            color: "#00ffe3"
-                        },
+                                offset: 0,
+                                color: "#00ffe3"
+                            },
                             {
                                 offset: 1,
                                 color: "#7ab3ae"
@@ -205,9 +203,9 @@ function realTime_error_bar(fixedData,errorData){
                     normal: {
                         barBorderRadius: 5,
                         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                            offset: 0,
-                            color: "red"
-                        },
+                                offset: 0,
+                                color: "red"
+                            },
                             {
                                 offset: 1,
                                 color: "#c66265"
@@ -222,44 +220,45 @@ function realTime_error_bar(fixedData,errorData){
     myChart.clear();
 
     myChart.setOption(option)
-    window.onresize = function(){
+    window.onresize = function() {
         myChart.resize();
     }
 
 
 }
-function realTime_error_machine(fixedData,errorData){
+
+function realTime_error_machine(fixedData, errorData) {
 
     let error_machine_dict = {}
-    for(let i in fixedData){
+    for (let i in fixedData) {
         let obj = fixedData[i];
-        if(obj['MachCode'] in error_machine_dict){
-            error_machine_dict[obj['MachCode']]['fixed']+=1;
-        } else{
-            error_machine_dict[obj['MachCode']] = {'MachCode':obj['MachCode'],'Description':obj['description'],'fixed':1,'unfixed':0}
+        if (obj['MachCode'] in error_machine_dict) {
+            error_machine_dict[obj['MachCode']]['fixed'] += 1;
+        } else {
+            error_machine_dict[obj['MachCode']] = { 'MachCode': obj['MachCode'], 'Description': obj['description'], 'fixed': 1, 'unfixed': 0 }
         }
     }
-    for(let j in errorData){
+    for (let j in errorData) {
         let e_obj = errorData[j];
-        if(e_obj['MachCode'] in error_machine_dict){
-            error_machine_dict[e_obj['MachCode']]['unfixed']+=1;
-        } else{
-            error_machine_dict[e_obj['MachCode']] = {'MachCode':e_obj['MachCode'],'Description':e_obj['description'],'fixed':0,'unfixed':1}
+        if (e_obj['MachCode'] in error_machine_dict) {
+            error_machine_dict[e_obj['MachCode']]['unfixed'] += 1;
+        } else {
+            error_machine_dict[e_obj['MachCode']] = { 'MachCode': e_obj['MachCode'], 'Description': e_obj['description'], 'fixed': 0, 'unfixed': 1 }
         }
 
     }
-    let new_error_machine_dict={}
+    let new_error_machine_dict = {}
     let total_count = 0;
-    for(let m in error_machine_dict){
+    for (let m in error_machine_dict) {
         new_error_machine_dict[m] = error_machine_dict[m]
     }
 
-    let res_counts = Object.keys(new_error_machine_dict).sort(function(a,b){
-        return new_error_machine_dict[b]['fixed']-new_error_machine_dict[a]['fixed'];
+    let res_counts = Object.keys(new_error_machine_dict).sort(function(a, b) {
+        return new_error_machine_dict[b]['fixed'] - new_error_machine_dict[a]['fixed'];
     });
     let y_fixed_data = [];
     let y_unfixed_data = [];
-    res_counts.forEach(function(d){
+    res_counts.forEach(function(d) {
         let item = new_error_machine_dict[d]
         y_fixed_data.push(parseInt(item['fixed']));
         y_unfixed_data.push(parseInt(item['unfixed']))
@@ -271,7 +270,7 @@ function realTime_error_machine(fixedData,errorData){
     y_fixed_data = y_fixed_data.slice(0, 20);
     y_unfixed_data = y_unfixed_data.slice(0, 20);
 
-    res_counts=res_counts.map(item=>'#'+item.toString())
+    res_counts = res_counts.map(item => '#' + item.toString())
     if (document.getElementById('error_machine_bar') != null) {
         echarts.dispose(document.getElementById('error_machine_bar'))
     }
@@ -282,8 +281,8 @@ function realTime_error_machine(fixedData,errorData){
         title: {
             left: 'center',
             text: '机器故障分布(前 20)',
-            textStyle:{//标题内容的样式
-                color:'dodgerblue',
+            textStyle: { //标题内容的样式
+                color: 'dodgerblue',
 
             },
         },
@@ -291,16 +290,16 @@ function realTime_error_machine(fixedData,errorData){
             trigger: 'axis',
             showDelay: 20,
             hideDelay: 20,
-            transitionDuration:0,
-            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            transitionDuration: 0,
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
             }
         },
         legend: {
             data: ['已修复故障', '故障正在发生'],
-            top:"12%",
-            textStyle:{//标题内容的样式
-                color:'dodgerblue',
+            top: "12%",
+            textStyle: { //标题内容的样式
+                color: 'dodgerblue',
 
             },
         },
@@ -310,41 +309,37 @@ function realTime_error_machine(fixedData,errorData){
             bottom: '3%',
             containLabel: true
         },
-        xAxis: [
-            {
-                type: 'category',
-                data: res_counts,
-                axisLabel: {
-                    show: true, //默认为true，设为false后下面都没有意义了
-                    interval: 0, //此处关键， 设置文本标签全部显示
-                },
+        xAxis: [{
+            type: 'category',
+            data: res_counts,
+            axisLabel: {
+                show: true, //默认为true，设为false后下面都没有意义了
+                interval: 0, //此处关键， 设置文本标签全部显示
+            },
 
-                axisLine:{
-                    lineStyle:{
-                        color:'#22cab7',
-                        width:2,
-                    }
+            axisLine: {
+                lineStyle: {
+                    color: '#22cab7',
+                    width: 2,
                 }
             }
-        ],
-        yAxis: [
-            {
-                type: 'value',
+        }],
+        yAxis: [{
+            type: 'value',
 
-                nameTextStyle:{
-                    color:['#be7012']
+            nameTextStyle: {
+                color: ['#be7012']
+            },
+            axisLine: {
+                lineStyle: {
+                    color: '#ebf8ac',
+                    width: 2
                 },
-                axisLine: {
-                    lineStyle:{
-                        color:'#ebf8ac',
-                        width:2
-                    },
-
-                }
-
 
             }
-        ],
+
+
+        }],
         series: [
 
             {
@@ -367,9 +362,9 @@ function realTime_error_machine(fixedData,errorData){
                     normal: {
                         barBorderRadius: 5,
                         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                            offset: 0,
-                            color: "#8ac47d"
-                        },
+                                offset: 0,
+                                color: "#8ac47d"
+                            },
                             {
                                 offset: 1,
                                 color: "#bdd7ac"
@@ -395,7 +390,7 @@ function realTime_error_machine(fixedData,errorData){
                     },
                 },
                 data: y_unfixed_data,
-                itemStyle: {color: 'red'}
+                itemStyle: { color: 'red' }
 
             },
 
@@ -405,33 +400,33 @@ function realTime_error_machine(fixedData,errorData){
     myChart.clear();
 
     myChart.setOption(option);
-    myChart.on('click', function (params) {
+    myChart.on('click', function(params) {
 
-        let select_data = fixedData.filter(item=>item['MachCode']==params['name'].slice(1))
-        for(let i in errorData){
-            if (errorData[i]['MachCode']==params['name'].slice(1)){
+        let select_data = fixedData.filter(item => item['MachCode'] == params['name'].slice(1))
+        for (let i in errorData) {
+            if (errorData[i]['MachCode'] == params['name'].slice(1)) {
                 select_data.push(errorData[i]);
             }
         }
         let select_error_data = {}
-        for(let m in select_data){
-            if (select_data[m]['StopCode'] in select_error_data){
-                select_error_data[select_data[m]['StopCode']]['count']+=1;
-            }else{
-                select_error_data[select_data[m]['StopCode']] = {'stopCode':select_data[m]['StopCode'],'description':select_data[m]['description'],'count':1}
+        for (let m in select_data) {
+            if (select_data[m]['StopCode'] in select_error_data) {
+                select_error_data[select_data[m]['StopCode']]['count'] += 1;
+            } else {
+                select_error_data[select_data[m]['StopCode']] = { 'stopCode': select_data[m]['StopCode'], 'description': select_data[m]['description'], 'count': 1 }
             }
         }
-        let res="";
-        let res_down = Object.keys(select_error_data).sort(function(a,b){
-            return select_error_data[b]['count']-select_error_data[a]['count'];
+        let res = "";
+        let res_down = Object.keys(select_error_data).sort(function(a, b) {
+            return select_error_data[b]['count'] - select_error_data[a]['count'];
         });
-        for(let i in res_down){
+        for (let i in res_down) {
             let obj = select_error_data[res_down[i]];
-            res += '故障次数:'+obj['count']+"--"+obj['description']+'\n'
+            res += '故障次数:' + obj['count'] + "--" + obj['description'] + '\n'
         }
         alert(res)
     })
-    window.onresize = function(){
+    window.onresize = function() {
         myChart.resize();
     }
 
--- walt_chn/php/send_post.php
+++ walt_chn/php/send_post.php
@@ -3,8 +3,8 @@
 
 require('db_info.php'); //数据库连接信息请在这个文件里改
 
-$serverName = "D25W0333\\SQLEXPRESS";
-$connectionInfo = array( "Database"=>"operator_log","UID"=>"Nautilus", "PWD"=>"MasterUser78");
+// $serverName = "D25W0333\\SQLEXPRESS";
+// $connectionInfo = array( "Database"=>"operator_log","UID"=>"Nautilus", "PWD"=>"MasterUser78");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 if( $conn === false ) {
      die( print_r( sqlsrv_errors(), true));
@@ -14,7 +14,6 @@ date_default_timezone_set('America/Denver');
 $today = date('m/d/Y H:i:s', time());
 $sql = "INSERT INTO [operator_log].[dbo].[log_table] ([sender],[receiver],[machCode],[stopCode],[details],[dateRec],[FileName]) VALUES (?, ?, ?, ?, ?, ?,?)";
 $params = array($_POST['post_Sender'],$_POST['post_Receiver'], $_POST['post_MachineId'], $_POST['post_StopCode'], $_POST['post_details'],$today,$_POST['post_FileName']);
-
 $stmt = sqlsrv_query( $conn, $sql, $params);
 if( $stmt === false ) {
      die( print_r( sqlsrv_errors(), true));
